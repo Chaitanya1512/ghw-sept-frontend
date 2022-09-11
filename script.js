a70e0e3c7d975bad19956fe5caea6fb22245d314
Chaitanya Thakur <>
2022-09-11 13:55:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const pKeys = document.querySelectorAll('.key');
+
+function playSound(newUrl) {
+  new Audio(newUrl).play()
+  console.log(newUrl)
+}
+
+pKeys.forEach((pKey, i) => {
+  const newUrl = 'notes/' + (i+1) + '.mp3'
+  pKey.addEventListener('click', () => playSound(newUrl))
+})
